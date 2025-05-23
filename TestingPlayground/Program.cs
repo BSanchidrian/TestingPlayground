1e32a36086609e92dc17485b0542cb8070069c96
BSanchidrian <borja.sanchidrian@datadoghq.com>
2025-05-23 13:47:40 +0200

    First commit

---
+++ TestingPlayground/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+public class Calculator
+{
+    public int Add(params int[] nums)
+    {
+        return nums.Sum();
+    }
+}
---
+++ UnitTests/CalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+
+namespace UnitTests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_SomeNumbers_ShouldWork()
+    {
+        int[] numbers = [1, 2, 10];
+        var calculator = new Calculator();
+
+        var result = calculator.Add(numbers);
+
+        result.Should().Be(numbers.Sum());
+    }
+}
